4989f1e7c72db3a196594c069ad8aafc43edd7b2
woodoyeon <162427815+woodoyeon@users.noreply.github.com>
2025-05-28 00:24:34 +0900

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,7 +11,11 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173', 'https://ai-shop-automation-project.vercel.app'],
+  credentials: true
+}));
+
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
